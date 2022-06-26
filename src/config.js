0837d12f6100b11ecf6f03dbf4a257de87a1820f
arifrhm <arif.rahman.rhm@gmail.com>
2022-06-26 22:42:57 +0700

    Update app name, config api, add cors package

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Replace the string with your private API key for the News API.
 // You can get one for free if you register at: https://newsapi.org/register
 
-export const NEWS_API_KEY = "YOUR_PRIVATE_API_KEY";
+export const NEWS_API_KEY = "f14a4c4312d0401eb354cd0d3739f8f1";
